b729cc72688a2f0cf01ae42cd2d714238d4ae9f0
iwannadraw <35878885+iwannadraw@users.noreply.github.com>
2018-01-27 19:14:15 -0500

    Movement in progress

--- Morse Escape Project/Assets/Scripts/Player_Control.cs
+++ Morse Escape Project/Assets/Scripts/Player_Control.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Control : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    public float speed = 2;
+    private Rigidbody2D rb;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+
+         float moveHoritzontal = 0f;
+
+         float moveVertical = 0f;
+
+         moveHoritzontal = Input.GetAxis("Horizontal");
+         moveVertical = Input.GetAxis("Vertical");
+
+         Vector2 movement = new Vector2(moveHoritzontal, moveVertical);
+
+         rb.AddForce(movement * speed);
+        
+
+
+    }
 
 
 
