233153e5b983cf290dec86277ae8c518f5e1db23
iwannadraw <35878885+iwannadraw@users.noreply.github.com>
2018-01-27 20:12:20 -0500

    Working Movement

--- Morse Escape Project/Assets/Scripts/Player_Control.cs
+++ Morse Escape Project/Assets/Scripts/Player_Control.cs
@@ -3,37 +3,45 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player_Control : MonoBehaviour {
 
 
-    public float speed = 2;
+    public float speed = 0.04f;
     private Rigidbody2D rb;
 
 
     // Use this for initialization
-    void Start () {
+    
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-
-         float moveHoritzontal = 0f;
-
-         float moveVertical = 0f;
-
-         moveHoritzontal = Input.GetAxis("Horizontal");
-         moveVertical = Input.GetAxis("Vertical");
-
-         Vector2 movement = new Vector2(moveHoritzontal, moveVertical);
-
-         rb.AddForce(movement * speed);
-        
-
-
     }
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)){
+            transform.Translate(Vector2.right * speed);
 
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed);
 
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed);
+
+        }
+
+    }
 }
+
+
