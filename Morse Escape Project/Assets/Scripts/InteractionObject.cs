7f775b5bcb15b17db2ddfb0e72015977255528d7
iwannadraw <35878885+iwannadraw@users.noreply.github.com>
2018-01-27 23:54:51 -0500

    updated scripts

---
+++ Morse Escape Project/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour {
+
+    public GameObject currentInterObj = null;
+
+    void OnTriggerEnter2D(Collider2d other)
+    {
+        if (other.CompareTag("interObject")) {
+            text = "This is item";
+
+            currentInterObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(COllider2D other) {
+        if (other.CompareTag("interObject"))
+        {   
+            if(other.GameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+            
+            
+        }
+    }
+}
